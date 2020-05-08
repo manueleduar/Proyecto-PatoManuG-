8119dc3226fee483fc995c4f7f40257d705f88c2
Gabriel Carrion <gabo_c_v@hotmail.com>
2020-05-08 03:51:50 -0500

    working semantic cube done all elements not declared in document table mark error

--- ply-3.11/cube.py
+++ ply-3.11/cube.py
@@ -1,68 +1,82 @@
 class Cube:
     def __init__(self):
-        self.operators ={
-            1:'+',
-            2:'-',
-            3:'*',
-            4:'/',
-            5:'<',
-            6:'>',
-            7:'<=',
-            8:'>=',
-            9:'==',
+        self.operators = {
+            1: '+',
+            2: '-',
+            3: '*',
+            4: '/',
+            5: '<',
+            6: '>',
+            7: '<=',
+            8: '>=',
+            9: '==',
             10: '!=',
-            11:'&&',
-            12:'|' 
+            11: '&&',
+            12: '|'
             }
-        self.types= {
-            1:'int',
-            2:'float',
-            3:'char',
-            4:'bool',
-            5:'CTEI',
-            6:'CTEF',
-            7:'CTEC',
-            8:'CTESTRING',
-            9:'ERROR',
-            } 
-        
-    def cuboSemantico(self):
-        self.compat={
-            #int
-            self.types[1]:{
-                #int-int compatibility
-                self.types[1]:{
-                    self.operators[1]:self.types[1],
-                    self.operators[2]:self.types[1],
-                    self.operators[3]:self.types[1],
-                    self.operators[4]:self.types[1],
-                    self.operators[5]:self.types[4],
-                    self.operators[6]:self.types[4],
-                    self.operators[7]:self.types[4],
-                    self.operators[8]:self.types[4],
-                    self.operators[9]:self.types[4],
-                    self.operators[10]:self.types[4],
-                    self.operators[11]:self.types[4],
-                    self.operators[12]:self.types[4],
+        self.types = {
+            1: 'int',
+            2: 'float',
+            3: 'char',
+            4: 'bool',
+            5: 'CTEI',
+            6: 'CTEF',
+            7: 'CTEC',
+            8: 'CTESTRING',
+            9: 'ERROR',
+            }
+
+        self.compat = {
+            # int
+            self.types[1]: {
+                # int-int compatibility
+                self.types[1]: {
+                    self.operators[1]: self.types[1],
+                    self.operators[2]: self.types[1],
+                    self.operators[3]: self.types[1],
+                    self.operators[4]: self.types[1],
+                    self.operators[5]: self.types[4],
+                    self.operators[6]: self.types[4],
+                    self.operators[7]: self.types[4],
+                    self.operators[8]: self.types[4],
+                    self.operators[9]: self.types[4],
+                    self.operators[10]: self.types[4],
+                    self.operators[11]: self.types[4],
+                    self.operators[12]: self.types[4],
                 },
-                #int-float compatibility
-                self.types[2]:{
-                    self.operators[1]:self.types[2],
-                    self.operators[2]:self.types[2],
-                    self.operators[3]:self.types[2],
-                    self.operators[4]:self.types[2],
-                    self.operators[5]:self.types[4],
-                    self.operators[6]:self.types[4],
-                    self.operators[7]:self.types[4],
-                    self.operators[8]:self.types[4],
-                    self.operators[9]:self.types[4],
-                    self.operators[10]:self.types[4],
-                    self.operators[11]:self.types[4],
-                    self.operators[12]:self.types[4],
-                    
+                # int-float compatibility
+                self.types[2]: {
+                    self.operators[1]: self.types[2],
+                    self.operators[2]: self.types[2],
+                    self.operators[3]: self.types[2],
+                    self.operators[4]: self.types[2],
+                    self.operators[5]: self.types[4],
+                    self.operators[6]: self.types[4],
+                    self.operators[7]: self.types[4],
+                    self.operators[8]: self.types[4],
+                    self.operators[9]: self.types[4],
+                    self.operators[10]: self.types[4],
+                    self.operators[11]: self.types[4],
+                    self.operators[12]: self.types[4],
+
                 },
-                #int-char compatibility
-                self.types[3]:{
+                # int-char compatibility
+                self.types[3]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[9],
+                    self.operators[10]: self.types[9],
+                    self.operators[11]: self.types[9],
+                    self.operators[12]: self.types[9],
+                },
+                 #int-bool compatibility
+                self.types[4]:{
                     self.operators[1]:self.types[9],
                     self.operators[2]:self.types[9],
                     self.operators[3]:self.types[9],
@@ -75,44 +89,126 @@ class Cube:
                     self.operators[10]:self.types[9],
                     self.operators[11]:self.types[9],
                     self.operators[12]:self.types[9],
+                    
+                },
+
+            },
+            # float
+            self.types[2]: {
+                    # float-int compatibility
+                self.types[1]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[4],
+                    self.operators[10]: self.types[4],
+                    self.operators[11]: self.types[4],
+                    self.operators[12]: self.types[4],
+
+                },
+                # float-float compatibility
+                self.types[2]: {
+                    self.operators[1]: self.types[2],
+                    self.operators[2]: self.types[2],
+                    self.operators[3]: self.types[2],
+                    self.operators[4]: self.types[2],
+                    self.operators[5]: self.types[4],
+                    self.operators[6]: self.types[4],
+                    self.operators[7]: self.types[4],
+                    self.operators[8]: self.types[4],
+                    self.operators[9]: self.types[4],
+                    self.operators[10]: self.types[4],
+                    self.operators[11]: self.types[4],
+                    self.operators[12]: self.types[4],
+
+                },
+                # float-char compatibility
+                self.types[3]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[9],
+                    self.operators[10]: self.types[9],
+                    self.operators[11]: self.types[9],
+                    self.operators[12]: self.types[9],
+                },
+                 #bool-float compatibility
+                self.types[4]:{
+                    self.operators[1]:self.types[9],
+                    self.operators[2]:self.types[9],
+                    self.operators[3]:self.types[9],
+                    self.operators[4]:self.types[9],
+                    self.operators[5]:self.types[9],
+                    self.operators[6]:self.types[9],
+                    self.operators[7]:self.types[9],
+                    self.operators[8]:self.types[9],
+                    self.operators[9]:self.types[9],
+                    self.operators[10]:self.types[9],
+                    self.operators[11]:self.types[9],
+                    self.operators[12]:self.types[9],
+                    
                 },
             },
-            #float
-            self.types[2]:{
-                    #float-int compatibility
-                self.types[1]:{
-                    self.operators[1]:self.types[9],
-                    self.operators[2]:self.types[9],
-                    self.operators[3]:self.types[9],
-                    self.operators[4]:self.types[9],
-                    self.operators[5]:self.types[9],
-                    self.operators[6]:self.types[9],
-                    self.operators[7]:self.types[9],
-                    self.operators[8]:self.types[9],
-                    self.operators[9]:self.types[4],
-                    self.operators[10]:self.types[4],
-                    self.operators[11]:self.types[4],
-                    self.operators[12]:self.types[4],
-                
+            # char
+            self.types[3]: {
+                    # char-int compatibility
+                self.types[1]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[9],
+                    self.operators[10]: self.types[9],
+                    self.operators[11]: self.types[9],
+                    self.operators[12]: self.types[9],
                 },
-                #float-char compatibility
-                self.types[2]:{
-                    self.operators[1]:self.types[2],
-                    self.operators[2]:self.types[2],
-                    self.operators[3]:self.types[2],
-                    self.operators[4]:self.types[2],
-                    self.operators[5]:self.types[4],
-                    self.operators[6]:self.types[4],
-                    self.operators[7]:self.types[4],
-                    self.operators[8]:self.types[4],
-                    self.operators[9]:self.types[4],
-                    self.operators[10]:self.types[4],
-                    self.operators[11]:self.types[4],
-                    self.operators[12]:self.types[4],
-                    
+                # char-float compatibility
+                self.types[2]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[9],
+                    self.operators[10]: self.types[9],
+                    self.operators[11]: self.types[9],
+                    self.operators[12]: self.types[9],
+
                 },
-                #float-char compatibility
-                self.types[3]:{
+                # char-char compatibility
+                self.types[3]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[4],
+                    self.operators[10]: self.types[4],
+                    self.operators[11]: self.types[4],
+                    self.operators[12]: self.types[4],
+                },
+                 #char-bool compatibility
+                self.types[4]:{
                     self.operators[1]:self.types[9],
                     self.operators[2]:self.types[9],
                     self.operators[3]:self.types[9],
@@ -125,65 +221,97 @@ class Cube:
                     self.operators[10]:self.types[9],
                     self.operators[11]:self.types[9],
                     self.operators[12]:self.types[9],
+                    
                 },
-                
+
             },
-            #char
-            self.types[3]:{
-                    #char-int compatibility
-                self.types[1]:{
-                    self.operators[1]:self.types[9],
-                    self.operators[2]:self.types[9],
-                    self.operators[3]:self.types[9],
-                    self.operators[4]:self.types[9],
-                    self.operators[5]:self.types[9],
-                    self.operators[6]:self.types[9],
-                    self.operators[7]:self.types[9],
-                    self.operators[8]:self.types[9],
-                    self.operators[9]:self.types[9],
-                    self.operators[10]:self.types[9],
-                    self.operators[11]:self.types[9],
-                    self.operators[12]:self.types[9],
+            # BOOLEAN
+            self.types[4]: {
+                # bool-int COMPAT
+                self.types[1]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[9],
+                    self.operators[10]: self.types[9],
+                    self.operators[11]: self.types[9],
+                    self.operators[12]: self.types[9],
+
                 },
-                #char-float compatibility
-                self.types[2]:{
-                    self.operators[1]:self.types[9],
-                    self.operators[2]:self.types[9],
-                    self.operators[3]:self.types[9],
-                    self.operators[4]:self.types[9],
-                    self.operators[5]:self.types[9],
-                    self.operators[6]:self.types[9],
-                    self.operators[7]:self.types[9],
-                    self.operators[8]:self.types[9],
-                    self.operators[9]:self.types[9],
-                    self.operators[10]:self.types[9],
-                    self.operators[11]:self.types[9],
-                    self.operators[12]:self.types[9],
-                    
+                # bool-float compatibility
+                self.types[2]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[9],
+                    self.operators[10]: self.types[9],
+                    self.operators[11]: self.types[9],
+                    self.operators[12]: self.types[9],
+
                 },
-                #int-char compatibility
-                self.types[3]:{
-                    self.operators[1]:self.types[9],
-                    self.operators[2]:self.types[9],
-                    self.operators[3]:self.types[9],
-                    self.operators[4]:self.types[9],
-                    self.operators[5]:self.types[9],
-                    self.operators[6]:self.types[9],
-                    self.operators[7]:self.types[9],
-                    self.operators[8]:self.types[9],
-                    self.operators[9]:self.types[4],
-                    self.operators[10]:self.types[4],
-                    self.operators[11]:self.types[4],
-                    self.operators[12]:self.types[4],
+                # bool-char compatibility
+                self.types[3]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[9],
+                    self.operators[6]: self.types[9],
+                    self.operators[7]: self.types[9],
+                    self.operators[8]: self.types[9],
+                    self.operators[9]: self.types[9],
+                    self.operators[10]: self.types[9],
+                    self.operators[11]: self.types[9],
+                    self.operators[12]: self.types[9],
                 },
-                
+                # bool bool compatibility
+                self.types[4]: {
+                    self.operators[1]: self.types[9],
+                    self.operators[2]: self.types[9],
+                    self.operators[3]: self.types[9],
+                    self.operators[4]: self.types[9],
+                    self.operators[5]: self.types[4],
+                    self.operators[6]: self.types[4],
+                    self.operators[7]: self.types[4],
+                    self.operators[8]: self.types[4],
+                    self.operators[9]: self.types[4],
+                    self.operators[10]: self.types[4],
+                    self.operators[11]: self.types[4],
+                    self.operators[12]: self.types[4],
+                },
             },
 
-        }    
-    
-    def printO(self):
-        print(self.operators[1])
-        print(self.types[1])
-        
+        }
+
+    def getTipo(self, izq, der, op):
+       return self.compat[izq][der][op]
+
+    def printO(self, izq, der, op):
+        print("valor de retorno es:")
+        print(self.getTipo(izq, der, op))
+
+
 x = Cube()
-x.printO()
+print("el primer operando es de tipo:", x.types[4])
+print("el segundo operando es de tipo:", x.types[4])
+print("el operador es:", x.operators[1])
+x.printO(x.types[4], x.types[4], x.operators[1])
+
+print("el primer operando es de tipo:", x.types[1])
+print("el segundo operando es de tipo:", x.types[2])
+print("el operador es:", x.operators[3])
+x.printO(x.types[1], x.types[2], x.operators[3])
+
+
+
+
