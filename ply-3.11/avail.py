2d0ba76ba53db8f09a805d91504d56bf8aa9db48
Gabriel Carrion <gabo_c_v@hotmail.com>
2020-05-16 16:28:30 -0500

    initial sandbox quads2

---
+++ ply-3.11/avail.py
@@ -0,0 +1,9 @@
+class Avail(object):
+    def __init__(self):
+        self.avail = 0
+        self.tmem = 'T'
+    def next(self):
+        self.avail += 1
+        return self.tmem + str(self.avail)
+    def clear(self):
+        self.avail = 0
--- ply-3.11/operaciones.py
+++ ply-3.11/operaciones.py
@@ -1,77 +1,81 @@
+from tablaFuncionesVariables import var
 from cube import Cube
 from stack import Stack
-#Clase cuadruplo 
+from avail import Avail
+# Clase cuadruplo
+
+
 class Quad():
     def __init__(self):
         self.quad = []
         self.cont = 0
-        
-    def addQ(self, right, left, op):
-        #añade un set de operador y operando derecho e izq
-        q = {op, right, left}
+
+    def addQ(self, right, left, op, result):
+        # añade un set de operador y operando derecho e izq
+        q = {op, right, left, result}
         self.quad.append(q)
-        #añade el contador
+        # añade el contador
         self.cont += 1
-        #anadir saltos
-    def addJ(self, goto, result, place):
-        q ={goto, result, place}
+        # anadir saltos
+
+    def add_logic(self, goto, result, place, other):
+        q = {goto, result, place, other}
         self.quad.append(q)
+
     def getQ(self):
-        #obtiene el ultimo
+        # obtiene el ultimo
         l = self.quad.pop()
         return l
+    def fill_Quad(self, index):
+        temp = self.quad[index]
+        temp[3]= len(self.quad)
+        self.quad[index] = tuple(temp)
+        print ("cuadruplo", self.quad[index])
 
 class Operaciones():
     def __init__(self):
         self.cubo = Cube()
-
-    def suma(self, operaodres:Stack, operando_name_and_types:Stack, quad:Quad): 
-        if operaodres.top() == '+' or '-':
-            #es igual al ultimo añadido 
-            right_operand = operando_name_and_types.pop()
-            left_operand = operando_name_and_types.pop()
-            operador = operaodres.pop()
-
-            #verifica que los tipos concuerden accediendo al atributo de los operandos
-            res_type = self.cubo.getTipo(right_operand.tipo, left_operand.tipo, operador)
-
-            if (res_type != 'ERROR'):
-                #añade los operandos y operador para crear cuadruplo 
-                quad.addQ(right_operand, left_operand,operador)
-                var_temp =  quad.getQ()
-
-                print("se creo el nuevo quad")
-                # #añade el cuadruplo creado en stack de operadores
-
-                operando_name_and_types.push(var_temp)
-                print("se ha añadido a la pila de operandos")
-
-                #FALTA SI ALGUN OPERANDO FUERA TEMPORAL RETORNARLO A AVAIL#
-            else:
-                print("Type mismatch")
-    
-    def if_salto(self, operadores:Stack, operando_name_and_types:Stack, saltos:Stack, quad:Quad):
-        #si el ultimo operador metido a la cola fue uno de los siguientes
-         if operadores.top() == '>' or '<' or'<=' or '>=' or '==' or '!=':
-             #saca el ultimo operando metido a operandos
-             res_type = operando_name_and_types.pop()
-             #si el ultimo tipo no es de tipo booleano 
-
-             if (res_type.tipo != 'bool'):
-                print("Type mismatch")
-             else:
-                 #genera el cuadruplo con un pointer vacio, el resultado y el operador es un goto
-                quad.addQ("pointer", res_type, "goTo")
-                #se pone el salto
-                saltos.push(quad)
-               
+        self.avail = Avail()
+        
+    def operations(self, operaodres: Stack, operando_name_and_types: Stack, quad: Quad):
+        operador = operaodres.pop()
+        right_operand = operando_name_and_types.pop()
+        left_operand = operando_name_and_types.pop()
+         # verifica que los tipos concuerden accediendo al atributo de los operandos
+        res_type = self.cubo.getTipo(
+        right_operand.tipo, left_operand.tipo, operador)
+        if (res_type != 'ERROR'):
+            result = self.avail.next()
+            quad.addQ(right_operand,left_operand,operador, result)
+            operando_name_and_types.push(result)
+        else:
+            print("type mismatch")
+            SystemExit()
             
+    def operations_min(self, operaodres: Stack, operando_name_and_types: Stack, quad: Quad):
+        operator = operaodres.pop()
+        operand = operando_name_and_types.pop()
+        quad.addQ(None, None, operator, operand)
+        operando_name_and_types.push(operand)
+        
 
-print ("hi welcome to test")
+    def logic(self, operadores: Stack, operando_name_and_types: Stack, saltos: Stack, quad: Quad, goto):
+        tipx = operadores.pop().tipo
+        if tipx =='bool':
+            result = operando_name_and_types.pop()
+            quad.add_logic(goto, result, None, -1)
+            saltos.push(len(quad)-1)
+            print("tipo cuad " , result.id)
+        else:
+            print("type mismatch")
+            SystemExit()
+   
+
+
+print("hi welcome to test")
 
 s = Stack()
 z = Stack()
-from tablaFuncionesVariables import var
 
 uno = 1
 dos = 2
@@ -79,22 +83,14 @@ cubo = Cube()
 a = var('float', uno)
 b = var('float', dos)
 
-print("a", a.tipo,"b", b.tipo)
 
 s.push(a)
 s.push(b)
 z.push('+')
 
-o = Operaciones()
-q = Quad()
+print(s.pop().tipo, s.pop().tipo, z.pop())
 
-# o.suma(z,s,q)
-
-
-print('PROBANDO PILAS')
-
-
-print(s.pop().tipo)
-print(s.pop().tipo)
+# o = Operaciones()
+# q = Quad()
 
 
