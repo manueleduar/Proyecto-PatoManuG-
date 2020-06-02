b78683c730342e6ab66f6e1c49e6fcf4c817a4a0
Gabriel Carrion <gabo_c_v@hotmail.com>
2020-06-01 21:57:37 -0500

    linked list for arrays

--- ply-3.11/dimensiones.py
+++ ply-3.11/dimensiones.py
@@ -1,19 +1,27 @@
 class Node ():
-    def __init__(self, inferior, superior, o):
-        self.inferior = inferior
+    def __init__(self, superior, o):
         self. superior = superior
         self.o = o
         self.next = None
 
-# class Dims (Node):
+class Dims (Node):
+    def __init__(self):
+        self.head = None
+        self.prev = None
     
-    
-    
+    def push (self, superior, o):
+        nuevo = Node(superior, o)
+        
+        if self.head is None:
+            self.head = nuevo
+            return
+        last = self.head
+        while(last.next):
+            last = last.next
+            
+        last.next = nuevo
+        
    
-n = Node( 0, 5, 0)
 
-n.next
-
-print("primer nodo dim base ", n.inferior, "dim ultima", n.superior, "next", n.next)
 
 
